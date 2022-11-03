e57ce9d831dca318efec8c10b12976836bd7de90
Syed Rafay <rafaysyed637@gmail.com>
2022-11-03 06:13:12 +0500

    done

---
+++ app.js
@@ -0,0 +1,65 @@
+alert("You Have Passed");
+window.alert("PASSED");
+
+var abc = "hello world";
+alert(abc);
+
+var b = 10;
+alert(b);
+
+var a = 10;
+var b = 20;
+var c = a + b;
+var c = b - a;
+var c = b / a;
+var c = b * a;
+var c = a + b * a;
+var c = (a + b) * a;
+alert(c);
+
+var a;
+
+a = "Syed Rafay";
+alert(a);
+
+a = "ABCD";
+alert(a);
+
+// var ab = 10;
+// var bc = 20;
+
+// a++;
+// b--;
+
+// console.log(ab);
+// console.log(bc);
+
+var e = 10;
+
+var f = a++;
+
+console.log(e);
+console.log(f);
+
+var name = "nehal";
+alert(name);
+
+var number = "206";
+alert(number);
+
+var caseOty = "144";
+alert(caseOty);
+
+var whatsLeftOver = "9 % 3";
+alert(whatsLeftOver);
+
+var message = "Thaks ";
+var username = " rafay";
+var banger = " !";
+alert(message + username + banger);
+
+var message = "you ";
+var username = " passed";
+var banger = " !";
+var custoMess = message + username + banger;
+alert(custoMess);
